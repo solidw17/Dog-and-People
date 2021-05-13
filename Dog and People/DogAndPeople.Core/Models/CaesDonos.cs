34d4c3980353f1beeed732feab19f847d117cfcc
GustavoHRM <gustavohrm17@gmail.com>
2021-05-13 14:13:41 -0300

    CaesDonos Model Added

---
+++ Dog and People/DogAndPeople.Core/Models/CaesDonos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogAndPeople.Core.Models
+{
+    public class CaesDonos
+    {
+        public int Id_dono { get; set; }
+        public int Id_cao { get; set; }
+    }
+}
