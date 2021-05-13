2b171751a6ec38151f5e45148434e568c5a937ad
GustavoHRM <gustavohrm17@gmail.com>
2021-05-13 14:09:27 -0300

    Models added and packages updated

---
+++ Dog and People/DogAndPeople.Core/Models/Caes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogAndPeople.Core.Models
+{
+    public class Caes
+    {
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Nome do cão")]
+        public string Name { get; set; }
+        [Required]
+        [DisplayName("Raça do cão")]
+        public string Race { get; set; }
+    }
+}
---
+++ Dog and People/DogAndPeople.Core/Models/Dono.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogAndPeople.Core.Models
+{
+    public class Dono
+    {
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Nome do dono")]
+        public string Name { get; set; }
+    }
+}
